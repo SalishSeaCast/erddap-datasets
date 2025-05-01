c12dde141e3beb09d1ab1b310c5cdb9064b9f7ee
Doug Latornell <dlatornell@eoas.ubc.ca>
2025-05-01 12:22:13 -0700

    Drop trailing slash from JAVA_HOME export path

--- setenv.sh
+++ setenv.sh
@@ -1,4 +1,4 @@
-export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64/
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 export JAVA_OPTS='-server -Djava.awt.headless=true -Xmx64G -Xms64G'
 export TOMCAT_HOME=/opt/apache-tomcat-10.1.40
 export CATALINA_HOME=/opt/apache-tomcat-10.1.40
